4667f59804652a38cab97af39cad6df447302b5c
Oswaldo Morales <morales.oswaldo151@gmail.com>
2026-03-16 14:17:33 -0600

    Probando datos desde el codigo

--- main.py
+++ main.py
@@ -1,2 +1,58 @@
-print("Hola mundo")
-print("OOOO")
+import sys
+
+print("ciudad,temperatura_celsius,clasificacion")
+
+def procesar_linea(linea):
+    partes = linea.strip().split(',')
+    
+    if len(partes)!=3:
+        return None
+    
+    ciudad = partes[0]
+    temp_str = partes[1]
+    unidad= partes[2].strip().upper()
+
+    if unidad not in ['C','F']:
+        return None
+    
+    try:
+        temperatura = float(temp_str)
+    except ValueError:
+        return None
+    
+    if unidad == "F":
+        celsius = (temperatura-32)*5/9
+    else:
+        celsius = temperatura
+
+    if celsius <0:
+        clasificacion = "Congelante"
+    elif celsius <= 15:
+        clasificacion = "Frio"
+    elif celsius <= 25:
+        clasificacion = "Templado"
+    elif celsius <= 35:
+        clasificacion = "Calido"
+    else:
+        clasificacion= "Extremo"
+    
+    return(ciudad,celsius,clasificacion)
+
+lineas_prueba = [
+    "CDMX,22,C",
+    "Nueva York,50,F",
+    "ERROR,abc,C",
+    "Miami,95,X",
+    "Moscu,-10,C"
+]
+
+
+for linea in lineas_prueba:
+    resultado = procesar_linea(linea)
+    if resultado:
+        ciudad, celsius, clasificacion = resultado
+        print(f'{ciudad}, {celsius: .1f},{clasificacion}')
+    else:
+        print(f'[IGNORADA]{linea}')    
+
+    
